6d03ea7b5c29a341f1ab7a59d0e9fe531379eab8
Hanna Negash <hnegash.n@gmail.com>
2023-11-07 14:03:43 -0800

    Change codes for readable codes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-//Global variables
-const theURL = "http://localhost:3000/toys";
-const divContainer = document.querySelector("#toy-collection");
-const toyForm = document.querySelector(".add-toy-form");
-
 //Don't remove this code provided for you
 let addToy = false;
 
@@ -18,6 +13,11 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  //Global variables
+  const theURL = "http://localhost:3000/toys";
+  const divContainer = document.querySelector("#toy-collection");
+  const toyForm = document.querySelector(".add-toy-form");
 
   //Helper functions
 
@@ -51,15 +51,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //append new created elements to div for card
     divElement.append(h2, imgElement, pElement, btn);
+
+    //For patch to click
+    const btnLike = document.getElementById(`toy_${toyObj.id}`);
+    btnLike.addEventListener("click", () => handleAddLike(toyObj));
+  };
+
+  //Increase a Toy's Likes - When a user clicks on a toy's like button
+  const handleAddLike = toy => {
+    const likes = toy.likes + 1;
+    fetch(`${theURL}/${toy.id}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      method: "PATCH",
+      body: JSON.stringify({ likes }),
+    })
+      .then(res => res.json())
+      .then(() => {
+        toy.likes = likes; //increased in db.json when clicking on a toy's like button
+      });
   };
 
   /*1. On the index.html page, there is a div with the id "toy-collection." When the page loads, make a 'GET' request to fetch all the toy objects. With the response data, make a <div class="card"> for each toy and add it to the toy-collection div.
    */
+
   const fetchData = () => {
     fetch(theURL)
       .then(res => res.json())
-      .then(toy => toy.forEach(appendToyCard)) //Call appendToyCard to render a card for each toy
-      .catch(err => alert(err));
+      .then(toy => toy.forEach(appendToyCard)); //Call appendToyCard to render a card for each toy
   };
   fetchData();
 
@@ -69,30 +90,29 @@ A POST request should be sent to http://localhost:3000/toys and the new toy adde
 If the post is successful, the toy should be added to the DOM without reloading the page.
 In order to send a POST request via fetch(), give the fetch() a second argument of an object. This object should specify the method as POST and also provide the appropriate headers and the JSON data for the request. 
 */
-  const postToy = newToy => {
+  const handleNewToy = e => {
+    e.preventDefault();
+
+    //get form inputs with value
+    const newToy = {
+      name: e.target.name.value,
+      image: e.target.image.value,
+      likes: 0,
+    };
     fetch(theURL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
       }, //JSON data for the request, - using form inputs with value
-      body: JSON.stringify({
-        name: newToy.name.value,
-        image: newToy.image.value,
-        likes: 0,
-      }),
+      body: JSON.stringify(newToy),
     })
       .then(res => res.json())
       .then(newToyCard => appendToyCard(newToyCard)); //create a card for a new toy after triggering appendToyCard function when clicking submit button
+    e.target.reset();
   };
 
   //add submit event listener to form
   //New toy added to Andy's Toy Collection after clicking on a submit button ()
-  toyForm.addEventListener("submit", e => {
-    e.preventDefault();
-
-    postToy(e.target); //or postToy(toyForm) - //Trigger postToy function after clicking submit button and get a new toy card
-
-    e.target.reset();
-  });
+  toyForm.addEventListener("submit", handleNewToy);
 });
